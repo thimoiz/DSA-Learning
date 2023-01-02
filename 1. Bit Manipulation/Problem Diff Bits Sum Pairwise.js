63a78c3a033a898b07040c779bef30109bd9f246
ThiMoiz <email16moiz@gmail.com>
2023-01-02 22:59:58 +0500

    Change var

--- 1. Bit Manipulation/Problem Diff Bits Sum Pairwise.js
+++ 1. Bit Manipulation/Problem Diff Bits Sum Pairwise.js
@@ -1,14 +1,15 @@
 function bitSumPair(array) {
   let ans = 0;
+  let n = array.length;
   for (let i = 0; i < 32; i++) {
     let countOnes = 0;
     let mask = 1 << i;
-    for (let j = 0; j < array.length; j++) {
+    for (let j = 0; j < n; j++) {
       if ((array[j] & mask) !== 0) {
         countOnes++;
       }
     }
-    ans += countOnes * (array.length - countOnes) * 2;
+    ans += countOnes * (n - countOnes) * 2;
   }
   return ans;
 }
