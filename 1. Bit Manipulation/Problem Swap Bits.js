b8e2b3ba5ae52d2feadcfd75360c7451c280908e
ThiMoiz <email16moiz@gmail.com>
2023-01-16 13:55:13 +0500

    Modified

--- 1. Bit Manipulation/Problem Swap Bits.js
+++ 1. Bit Manipulation/Problem Swap Bits.js
@@ -3,12 +3,15 @@ function swapping(n, p1, p2) {
   p1 -= 1;
   p2 -= 1;
 
-  let bitP1 = n & (1 << p1);
-  let bitP2 = n & (1 << p2);
+  let mask1 = 1 << p1;
+  let mask2 = 1 << p2;
+
+  let bitP1 = n & mask1;
+  let bitP2 = n & mask2;
 
   if (bitP1 !== bitP2) {
-    n = n ^ (1 << p1);
-    n = n ^ (1 << p2);
+    n = n ^ mask1;
+    n = n ^ mask2;
   }
 
   return n;
